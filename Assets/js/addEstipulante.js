9869c3852863ee21cb5f80f6d3a213f915f123e5
ney-Rocha <rocha.brasil@hotmail.com>
2023-03-01 16:01:27 -0300

    correção de exibiçlão de img plugin

--- Assets/js/addEstipulante.js
+++ Assets/js/addEstipulante.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         Swal.fire({
             title: '<h3 class="text-danger text-uppercase">Atenção!</h3>',
             html: '<p> O subestipulante de número <span> 2 </span>, <strong> CNPJ <span>41.618.079/0001-04</span> </strong> está com restrição. Para continuar, exclua o estipulante ou mude o CNPJ.</p>',
-            imageUrl: '../Assets/img/icons/Error.png',
+            imageUrl: 'Assets/img/icons/Error.png',
             imageHeight:80,
             imageWidth:80,
             imageAlt: 'Exclamação de alerta!',
